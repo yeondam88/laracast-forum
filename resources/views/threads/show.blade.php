dc6e9a3dccbbb2511f11e9fab1f58e4938a86283
Yeondam Park <giyen9088@gmail.com>
2019-04-25 22:05:45 -0700

    adding replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,5 +12,20 @@
       </div>
     </div>
   </div>
+
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      @foreach($thread->replies as $reply)
+      <div class="card mt-4">
+        <div class="card-header">
+          <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
+        </div>
+        <div class="card-body">
+          {{ $reply->body }}
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </div>
 </div>
 @endsection
