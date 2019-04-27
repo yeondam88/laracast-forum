d65a2bcd3148accca82aba9dc28342beda802476
Yeondam Park <giyen9088@gmail.com>
2019-04-26 21:37:51 -0700

    work on form field

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -4,8 +4,10 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">{{ $thread->title }}</div>
-
+        <div class="card-header">
+        <a href="#">{{ $thread->creator->name }}</a> posted:
+          {{ $thread->title }}
+        </div>
         <div class="card-body">
           {{ $thread->body }}
         </div>
@@ -16,16 +18,23 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       @foreach($thread->replies as $reply)
-      <div class="card mt-4">
-        <div class="card-header">
-          <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
-        </div>
-        <div class="card-body">
-          {{ $reply->body }}
-        </div>
-      </div>
+        @include('threads.reply') 
       @endforeach
     </div>
   </div>
+
+  @if (auth()->check())
+  <div class="row justify-content-center mt-4">
+    <div class="col-md-8">
+      <form method="POST" action="{{ $thread->path() . '/replies' }}">
+        {{ csrf_field() }}
+        <div class="form-group">
+          <textarea name="body" id="body" class="form-control" rows="5" placeholder="Have something here.."></textarea>
+          <button class="btn btn-default mt-2" type="submit">Post</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  @endif
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
+
+
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
